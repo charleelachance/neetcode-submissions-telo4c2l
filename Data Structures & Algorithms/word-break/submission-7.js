bc78fe24b1f43a46b1b8ce882487a82ce25935b6
Charlee Lachance <74006847+charleelachance@users.noreply.github.com>
2026-03-30 14:09:41 -0500

    Add: word-break - submission-7

---
+++ Data Structures & Algorithms/word-break/submission-7.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string[]} wordDict
+     * @return {boolean}
+     */
+    wordBreak(s, wordDict) {
+        let dp = [];
+        let wordSet = new Set(wordDict);
+
+        function recurse(i) {
+            if (i === s.length) {
+                return true;
+            }
+            if (dp[i] !== undefined) return dp[i];
+            
+            for (let end=i; end<s.length; end++) {
+                if (wordSet.has(s.slice(i, end+1))) {
+                    dp[i] = recurse(end+1);
+                    if (dp[i] === true) return true;
+                }
+            }
+            return false;
+        }
+
+        return recurse(0) ?? false;
+    }
+}
