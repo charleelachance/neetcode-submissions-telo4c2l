6a750c77b1440386b726acaf8f31c763f39cc58a
Charlee Lachance <74006847+charleelachance@users.noreply.github.com>
2026-04-14 12:55:26 -0500

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        // brute force: try every pair of numbers until you find it
+
+        // iterate through nums to create a hash table
+        // iterate again and search for the completement that would reach target
+
+        const lookup = new Map();
+
+        // for (let i=0; i<nums.length; i++) {
+        //     const num = nums[i];
+        //     lookup.set(num, i); // stores the last instance
+        // }
+        nums.forEach((num, i) => {
+            lookup.set(num, i);
+        })
+
+        for (let i=0; i<nums.length; i++) {
+            const num = nums[i];
+            const difference = target - num;
+            const difInMap = lookup.get(difference);
+            if (difInMap && difInMap !== i) {
+                return [i, difInMap];
+            }
+        }
+    }
+}
