b68d62516da32824d78a23a0f0d4349178816449
Charlee Lachance <74006847+charleelachance@users.noreply.github.com>
2026-04-14 15:44:06 -0500

    Add: binary-search - submission-7

---
+++ Data Structures & Algorithms/binary-search/submission-7.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let start = 0;
+        let end = nums.length-1;
+
+        while (start<=end) {
+            let mid = start + Math.floor((end-start)/2);
+
+            if (nums[mid] === target) {
+                return mid;
+            }
+
+            if (nums[mid] <= target) {
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+}
